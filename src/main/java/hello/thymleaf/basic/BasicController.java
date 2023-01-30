5f2ce68d5057da0220c29d2cede72b8e0f308bfc
신창호 <vkdghckdh@gmail.com>
2023-01-30 18:30:49 +0900

    타임리프 javascript inline

--- src/main/java/hello/thymleaf/basic/BasicController.java
+++ src/main/java/hello/thymleaf/basic/BasicController.java
@@ -116,6 +116,12 @@ public class BasicController {
         addUsers(model);
         return "basic/block";
     }
+    @GetMapping("/javascript")
+    public String javascript(Model model){
+        model.addAttribute("user", new User("userA", 40));
+        addUsers(model);
+        return "basic/javascript";
+    }
 
     private void addUsers(Model model){
         List<User> list = new ArrayList<>();
