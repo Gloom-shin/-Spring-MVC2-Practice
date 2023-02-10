dafb8f720f25f809b5535638dd1e17a8b007e686
신창호 <vkdghckdh@gmail.com>
2023-02-10 14:30:51 +0900

    GraalVM Java-javaScript 엔진 맛보기

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,20 +1,23 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.graalvm.polyglot.Context;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+@ScriptAssert(lang = "javascript", script = "_this.startDate.before(_this.endDate)")
 public class Item {
 
     private Long id;
     @NotBlank
     private String itemName;
     @NotNull
-    @Range(min = 1000, max = 1000000, message = "{item.price.Range}")
+    @Range(min = 1000, max = 1000000)
     private Integer price;
     @NotNull
     @Max(9999)
@@ -27,5 +30,14 @@ public class Item {
         this.itemName = itemName;
         this.price = price;
         this.quantity = quantity;
+    }
+    public void checkBenefit(){
+        try (Context context = Context.create("js")) {
+
+            // 2 출력
+            context.eval("js", "_this.price * _this.quantity >= 10000");
+        } catch (Exception e) {
+            System.err.println();
+        }
     }
 }
--- validation/src/test/java/hello/itemservice/validation/BeanValidationTest.java
+++ validation/src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -1,13 +1,20 @@
 package hello.itemservice.validation;
 
 import hello.itemservice.domain.item.Item;
+
 import org.junit.jupiter.api.Test;
 
+
+import javax.script.ScriptEngineFactory;
+import javax.script.ScriptEngineManager;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import java.util.List;
 import java.util.Set;
+import org.graalvm.polyglot.*;
+import org.graalvm.polyglot.proxy.*;
 
 public class BeanValidationTest {
 
@@ -27,5 +34,16 @@ public class BeanValidationTest {
             System.out.println("violation = " + violation.getMessage());
 
         }
+    }
+    @Test
+    void engineCheck(){
+        try (Context context = Context.create("js")) {
+
+            // 2 출력
+            context.eval("js", "print( Math.min(2, 3) )");
+        } catch (Exception e) {
+            System.err.println();
+        }
+
     }
 }
